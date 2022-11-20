8dfc109ad3fcc6e0f1e0081c92156c0088f9e462
Adrian Kalinin <adriankalinin@protonmail.com>
2022-11-20 13:37:32 +0200

    Create basic command line runner

--- src/main/java/com/example/marketplace/MarketplaceApplication.java
+++ src/main/java/com/example/marketplace/MarketplaceApplication.java
@@ -1,13 +1,25 @@
 package com.example.marketplace;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MarketplaceApplication {
+	private static final Logger log = LoggerFactory.getLogger(MarketplaceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarketplaceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner createDemo() {
+		return (args) -> {
+			log.info("Hello, world!");
+		};
 	}
 
 }
