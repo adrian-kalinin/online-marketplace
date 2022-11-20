a8575cea5694298fa027395fdfa4d15960fe3b6c
Adrian Kalinin <adriankalinin@protonmail.com>
2022-11-20 11:35:00 +0200

    Create model and repository for User

---
+++ src/main/java/com/example/marketplace/model/User.java
@@ -0,0 +1,59 @@
+package com.example.marketplace.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "marketplace_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 16)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
