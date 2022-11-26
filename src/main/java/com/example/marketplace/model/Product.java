90a4abc3187c7ff0ce36af07355c8aaf6e2a90e2
Adrian Kalinin <adriankalinin@protonmail.com>
2022-11-26 22:59:07 +0200

    Create getters, setters, constructors, and toString() for Product model

--- src/main/java/com/example/marketplace/model/Product.java
+++ src/main/java/com/example/marketplace/model/Product.java
@@ -23,4 +23,65 @@ public class Product {
     @Column(name = "location", nullable = false, length = 120)
     private String location;
 
+    public Product() {
+    }
+
+    public Product(String title, String description, double price, String location) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", location='" + location + '\'' +
+                '}';
+    }
+
 }
