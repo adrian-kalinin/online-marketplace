93cf062e1f657342bf987c4c71bd7121d27b1a8a
Adrian Kalinin <adriankalinin@protonmail.com>
2022-11-28 01:04:16 +0200

    Implement profile page

--- src/main/java/com/example/marketplace/controller/MarketplaceController.java
+++ src/main/java/com/example/marketplace/controller/MarketplaceController.java
@@ -1,19 +1,32 @@
 package com.example.marketplace.controller;
 
+import com.example.marketplace.model.Product;
+import com.example.marketplace.model.User;
 import com.example.marketplace.repository.ProductRepository;
+import com.example.marketplace.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class MarketplaceController {
 
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public MarketplaceController(ProductRepository productRepository) {
+    public MarketplaceController(ProductRepository productRepository, UserRepository userRepository) {
         this.productRepository = productRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/")
@@ -25,6 +38,34 @@ public class MarketplaceController {
     public String feed(Model model) {
         model.addAttribute("products", productRepository.findAll());
         return "feed";
+    }
+
+    @GetMapping("/profile")
+    public String profile(Principal principal, Model model) {
+        String username = principal.getName();
+        User currentUser = userRepository.findByEmail(username);
+        List<Product> products = productRepository.findAllByUser(currentUser);
+
+        model.addAttribute("user", currentUser);
+        model.addAttribute("products", products);
+
+        return "profile";
+    }
+
+    @PostMapping("/profile/update")
+    public String profileEdit(@Valid @ModelAttribute("user") User user, Principal principal, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "profile";
+        }
+
+        String username = principal.getName();
+        User currentUser = userRepository.findByEmail(username);
+
+        if (Objects.equals(user.getId(), currentUser.getId())) {
+            userRepository.save(user);
+        }
+
+        return "redirect:/profile";
     }
 
 }
--- src/main/java/com/example/marketplace/repository/ProductRepository.java
+++ src/main/java/com/example/marketplace/repository/ProductRepository.java
@@ -1,7 +1,13 @@
 package com.example.marketplace.repository;
 
 import com.example.marketplace.model.Product;
+import com.example.marketplace.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllByUser(User user);
+
 }
