cca34ac7988dbe579577d98fcac79e87eb3ef076
Adrian Kalinin <adriankalinin@protonmail.com>
2022-12-01 03:02:11 +0200

    Add smoke testing

--- src/test/java/com/example/marketplace/MarketplaceApplicationTests.java
+++ src/test/java/com/example/marketplace/MarketplaceApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.marketplace;
 
+import com.example.marketplace.controller.AuthenticationController;
+import com.example.marketplace.controller.MarketplaceController;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class MarketplaceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private AuthenticationController authenticationController;
+
+    @Autowired
+    private MarketplaceController marketplaceController;
+
+    @Test
+    void contextLoads() {
+        assertThat(authenticationController).isNotNull();
+        assertThat(marketplaceController).isNotNull();
+    }
 
 }
